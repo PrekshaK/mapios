95ad5c26d5904ad5e07462a5b771d4a6e07c9f34
Preksha Koirala <prekshakoirala@gmail.com>
2016-02-24 20:16:37 -0500

    getting users location

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -1,43 +1,63 @@
 import UIKit
 import MapKit
+import CoreLocation
+
+
 
 
 var searchKey = ""
 let GOOGLE_API_KEY = "AIzaSyB9Yzf1rT5v01NZBiv4mj4HPzRr4AhOC0w"
-var baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.90,-77.016&radius=500&type=restaurant&name=\(searchKey)&key=\(GOOGLE_API_KEY)"
-//let session = NSURLSession.sharedSession()
-//let request = NSMutableURLRequest(URL: NSURL(string: baseURL)!)
-//let task = session.dataTaskWithRequest(request)
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, CLLocationManagerDelegate {
     
     
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
-
-    
-   
     
     @IBOutlet weak var keyword: UITextField!
     
     
+    
+    
+    var locValue:CLLocation!{
+        didSet{
+            //let coord = self.currentLocation.coordinate
+//            print (coord.latitude)
+//            print(coord.longitude)
+        }
+    }
+    
+    
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         
+        
+        //resigning as first responder
         keyword.resignFirstResponder()
         
+        
+        //remove previous annotations
         let annotationsToRemove = mapView.annotations.filter { $0 !== mapView.userLocation }
         mapView.removeAnnotations( annotationsToRemove )
-        
+
         
         searchKey = keyword.text!
         
+        
         print (searchKey)
         
-        baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.90,-77.016&radius=500&type=restaurant&name=\(searchKey)&key=\(GOOGLE_API_KEY)"
+        var baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(locValue.coordinate.latitude),\(locValue.coordinate.longitude)&radius=500&type=restaurant&name=\(searchKey)&key=\(GOOGLE_API_KEY)"
         
         
         
-        let center = CLLocationCoordinate2DMake(38.90, -77.016)
+        
+        
+        
+        
+        
+       // here we declare center and vicinity of the map
+        let center = CLLocationCoordinate2DMake(locValue.coordinate.latitude, locValue.coordinate.longitude)
         let region = MKCoordinateRegion(center:center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
         
         let session = NSURLSession.sharedSession()
@@ -50,23 +70,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 return
             }
             
-            //do conver to json
             
             do{
                 let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
                 
+                //we go through the jsonobject as results we get
+                
                 if let items = json["results"] as? [[String: AnyObject]]{
                     for items in items{
-                        //process items here
+                        
                         print (items)
                         
-                        if let name = items["name"] as? String, vicinity = items["vicinity"] as? String{
+                        
+                        
+                        if let name = items["name"] as? String, let vicinity = items["vicinity"] as? String{
                             
                             let coords: [Double] = self.getCoords(items["geometry"] as! Dictionary)
                             
                             
                             let annotation = MKPointAnnotation()
                             
+                            //defining annotations
                             annotation.coordinate = CLLocationCoordinate2D(
                                 latitude: coords[0],
                                 longitude: coords[1]
@@ -74,20 +98,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
                             
                             annotation.title = name + vicinity
                             
+                            //pinning annotations
+                            
                             self.mapView.addAnnotation(annotation)
                             
                         }
                         
                     }
-                    //when finished, update the UI on the main thread
+                    //updating UI on the main thread
                     dispatch_async(dispatch_get_main_queue()){
                         self.mapView.setRegion(region, animated: true)
                     }
                 }
-                
+
                 
             }catch{
-                //    print("error serializing json: \(error)")
                 print("error serializing json")
             }
             
@@ -102,21 +127,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     
     override func viewDidLoad() {
+        
+  
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        
+        
+        self.keyword.becomeFirstResponder()
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+    
         var center = CLLocationCoordinate2DMake(38.90, -77.016)
         var region = MKCoordinateRegion(center:center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
         
+        
+        //pinning annotations when view loads
         self.mapView.setRegion(region, animated: true)
         
         
     }
+    
+    
+
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print ("yes")
+        locValue = manager.location!
+        
+        //center = CLLocationCoordinate2DMake(locValue.latitude, locValue.longitude)
+        locationManager.stopUpdatingLocation()
+        
+        
+        print("locations = \(locValue.coordinate.latitude) \(locValue.coordinate.longitude)")
+    }
+    
     
     
     func getCoords(data: [String:AnyObject]) -> [Double]{
@@ -135,10 +183,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
 }
 
 
-
-//    let delta = 0.03
-//    let region = MKCoordinateRegion();
-//    region.center.latitude = 38.90;
-//    region.enter.longitude = -77.016
 
 
